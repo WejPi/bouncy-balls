f15ecd0ae98c8874141d6e7abf9a5fbf7841075e
Piotr <piotrwejda@gmail.com>
2024-10-14 13:42:44 +0200

    second commit

--- bouncy_balls.py
+++ bouncy_balls.py
@@ -1,6 +1,7 @@
 import pygame
 import pygame.display
 import pygame.draw
+import pygame.math
 import pygame.sprite
 from random import randint
 
@@ -24,20 +25,27 @@ class Main():
         self.HEIGHT = height
         self.SPEED_X = 0.3
         self.SPEED_Y = 0.3
+        self.RADIUS = 50
+        self.NUMBER_OF_BALLS = 3
     def components(self):
         self.window = pygame.display.set_mode((self.WIDTH,self.HEIGHT))
     def Loop(self):
         self.run = True
-        self.ball1 = Ball("green",self.WIDTH/2,self.HEIGHT/2,50)
+        self.balls = []
+        for i in range(self.NUMBER_OF_BALLS):
+            self.balls.append(Ball("green",self.WIDTH/2+randint(-10,10),self.HEIGHT/2+randint(-10,10),self.RADIUS))
 
-        self.ball1.x = randint(50,self.WIDTH-50)
-        self.ball1.y = randint(50,self.HEIGHT-50)
-        self.ball1.DIRECTION_X = randint(-1,1)
-        self.ball1.DIRECTION_Y = randint(-1,1)
-        if self.ball1.DIRECTION_X == 0:
-            self.ball1.DIRECTION_X = 1
-        if self.ball1.DIRECTION_Y == 0:
-            self.ball1.DIRECTION_Y = 1
+
+        for ball in self.balls:
+            ball.x = randint(self.RADIUS,self.WIDTH-self.RADIUS)
+            ball.y = randint(self.RADIUS,self.HEIGHT-self.RADIUS)
+            ball.DIRECTION_X = randint(-1,1)
+            ball.DIRECTION_Y = randint(-1,1)
+            if ball.DIRECTION_X == 0:
+                ball.DIRECTION_X = 1
+            if ball.DIRECTION_Y == 0:
+                ball.DIRECTION_Y = 1
+        
 
         while self.run == True:
             for event in pygame.event.get():
@@ -46,15 +54,26 @@ class Main():
             self.window.fill("black")
             
             
-            self.ball1.draw(self.window)
-            self.ball1.x += self.SPEED_X*self.ball1.DIRECTION_X
-            self.ball1.y += self.SPEED_Y*self.ball1.DIRECTION_Y
+            for ball in self.balls:
+                ball.draw(self.window)
+                ball.x += self.SPEED_X*ball.DIRECTION_X
+                ball.y += self.SPEED_Y*ball.DIRECTION_Y
 
-            if self.ball1.x<50 or self.ball1.x>750:
-                self.ball1.DIRECTION_X*=-1
-            if self.ball1.y<50 or self.ball1.y>550:
-                self.ball1.DIRECTION_Y*=-1
+                if ball.x<self.RADIUS or ball.x>self.WIDTH-self.RADIUS:
+                    ball.DIRECTION_X*=-1
+                if ball.y<self.RADIUS or ball.y>self.HEIGHT-self.RADIUS:
+                    ball.DIRECTION_Y*=-1
+                
+            
 
+            for i in range(len(self.balls)):
+                for j in range(i+1,len(self.balls)):
+                    dx = self.balls[i].x - self.balls[j].x
+                    dy = self.balls[i].y - self.balls[j].y
+                    distance = (dx**2+dy**2)**0.5
+                    if distance<2*self.RADIUS:
+                        self.balls[i].x, self.balls[j].x = self.balls[j].x, self.balls[i].x
+                        self.balls[i].y, self.balls[j].y = self.balls[j].y, self.balls[i].y
 
             pygame.display.update()
 
